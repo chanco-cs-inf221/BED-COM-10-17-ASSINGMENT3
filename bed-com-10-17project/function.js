f6364e5456187f9f32e3c4bff17bd7b07c76fa79
StevenKamwaza <48084787+StevenKamwaza@users.noreply.github.com>
2019-04-26 03:56:10 -0400

    Update function.js

--- bed-com-10-17project/function.js
+++ bed-com-10-17project/function.js
@@ -1,33 +1,37 @@
  function convert()
     {
-	  var username=document.getElementById("url").value;
+	//getting variables using id element
+      var username=document.getElementById("url").value;
       var  email = document.getElementById('em').value;
       var tem =document.getElementById('item').value;
-	  var pcode = document.getElementById('vista').value;
+      var pcode = document.getElementById('vista').value;
       var place =document.getElementById('type').value;
       var date = new Date(document.getElementById('Date').value);
 
 
     //storing item by item
 
-    localStorage.setItem('Username',username);
-    localStorage.setItem('Email',email);
-    localStorage.setItem('item',tem);
-    localStorage.setItem('Pcode',pcode);
-    localStorage.setItem('place',place);
-    localStorage.setItem('Date',date);
-    if (username === localStorage.getItem('Username')&& date === localStorage.getItem('Date')) {
+       localStorage.setItem('Username',username);
+       localStorage.setItem('Email',email);
+       localStorage.setItem('item',tem);
+       localStorage.setItem('Pcode',pcode);
+       localStorage.setItem('place',place);
+      localStorage.setItem('Date',date);
+      
+	    //avoiding duplicats
+      if (username === localStorage.getItem('Username')&& date === localStorage.getItem('Date')) {
 
         document.getElementById("converted_url").innerHTML=" You already booke the room on that day";
         form.reset();
       }
       else{
+	      //getting variables  from local storage
         var name = localStorage.getItem('Username');
         var Type = localStorage.getItem('place');
         var items= localStorage.getItem('item');
     
 
-               
+             //displaying items   
        document.getElementById("welcome").innerHTML= "Dear ,"+name +" you have succesfully buy "+items+" we will deriver it to you using your post code"+Type; 
         alert("Some money will be deducted from your account");
        form.reset(); 
